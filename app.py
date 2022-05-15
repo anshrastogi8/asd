beac27bf33da8e26ab719bea081a80289212aedb
anshrastogi8 <75429644+anshrastogi8@users.noreply.github.com>
2022-05-15 18:22:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,22 +13,7 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        preg = int(request.form['pregnancies'])
-        glucose = int(request.form['glucose'])
-        bp = int(request.form['bloodpressure'])
-        st = int(request.form['skinthickness'])
-        insulin = int(request.form['insulin'])
-        bmi = float(request.form['bmi'])
-        dpf = float(request.form['dpf'])
-        age = int(request.form['age'])
-        
-        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
-        my_prediction = classifier.predict(data)
-        
-        return render_template('result.html', prediction=my_prediction)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
