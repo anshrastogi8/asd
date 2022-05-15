0b0386f2bce2939b8d7cd2c12371890698a920a0
anshrastogi8 <75429644+anshrastogi8@users.noreply.github.com>
2022-05-15 18:26:38 +0530

    Update Diabetes Predictor - Deployment.py

--- Diabetes Predictor - Deployment.py
+++ Diabetes Predictor - Deployment.py
@@ -3,22 +3,11 @@ import numpy as np
 import pandas as pd
 import pickle
 
-# Loading the dataset
-df = pd.read_csv('kaggle_diabetes.csv')
+
 
 # Renaming DiabetesPedigreeFunction as DPF
 df = df.rename(columns={'DiabetesPedigreeFunction':'DPF'})
 
-# Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
-df_copy = df.copy(deep=True)
-df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
-
-# Replacing NaN value by mean, median depending upon distribution
-df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
-df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
-df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
-df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
-df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
 
 # Model Building
 from sklearn.model_selection import train_test_split
